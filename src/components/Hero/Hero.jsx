0ac1fcd11e2d96c810adf565de1f6c45d866895f
Ashiq <robiulislam.1711@gmail.com>
2024-11-19 23:41:24 +0600

    resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import './Hero.css'
 import profile_img from '../../assets/pp.png'
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import resume from '../../assets/Ashiq.dev.pdf'
 const Hero = () => {
   return (
     <div id='home' className='hero'>
@@ -11,7 +12,7 @@ const Hero = () => {
 <p>I'm a full stack developer specializing in building exceptional digital experiences. Currently, I'm focused on building responsive full-stack web applications.</p>
 <div className='hero-action'>
 <div className='hero-contact'>  <AnchorLink className="anchor-link" offset={50} href="#contact"><p>Contact With Me</p></AnchorLink> </div>
-<div className='hero-resume'>My resume <FaCloudDownloadAlt className='download-icon'/></div>
+<div className='hero-resume'> <a target='_blank' href={resume} download={resume}> My Resume <FaCloudDownloadAlt className='download-icon'/></a></div>
 </div>
     </div>
   )
