d795f602c40b8301461e138e24006c29cf30ff86
Ashiq <robiulislam.1711@gmail.com>
2024-11-20 12:28:16 +0600

    new

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,6 +1,9 @@
 import project1_img from '../assets/project_1.svg'
+import Hero_section from '../assets/Hero_section.png'
 import project2_img from '../assets/project_2.svg'
+import contact from '../assets/contact.png'
 import project3_img from '../assets/project_3.svg'
+import my_portfolio from '../assets/my_portfolio.png'
 import project4_img from '../assets/project_4.svg'
 import project5_img from '../assets/project_5.svg'
 import project6_img from '../assets/project_6.svg'
@@ -9,17 +12,17 @@ const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:Hero_section
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:my_portfolio
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:contact
     },
     {
         w_no:4,
@@ -35,6 +38,21 @@ const mywork_data = [
         w_no:6,
         w_name:"Web design",
         w_img:project6_img
+    },
+    {
+        w_no:7,
+        w_name:"Web design",
+        w_img:project1_img
+    },
+    {
+        w_no:8,
+        w_name:"Web design",
+        w_img:project2_img
+    },
+    {
+        w_no:9,
+        w_name:"Web design",
+        w_img:project3_img
     },
 ]
  
--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MyWork.css";
 import Title from "../Title/Title";
 import theme_pattern from "../../assets/theme_pattern.svg";
 import mywork_data from "../../assets/mywork_data";
 import arrow from "../../assets/arrow_icon.svg";
 const MyWork = () => {
+  const [visivle, setVisivle] =useState(3);
+  const showMore=()=>{
+    setVisivle(prevValue=>prevValue+3)
+  }
   return (
     <div id="portfolio" className="mywork">
       <div>
         <Title text="My Work" theme_pattern={theme_pattern} />
       </div>
       <div className="mywork-card">
-        {mywork_data.map((item, index) => {
+        {mywork_data.slice(0, visivle).map((item, index) => {
           return (
             <div key={index} className="mywork-card-item">
               <img src={item.w_img} alt="" />
@@ -21,7 +25,7 @@ const MyWork = () => {
         })}
       </div>
       <div className="showmore">
-        <p>Show more</p>
+        <p onClick={showMore}>Show more</p>
         <img src={arrow} alt="" />
       </div>
     </div>
